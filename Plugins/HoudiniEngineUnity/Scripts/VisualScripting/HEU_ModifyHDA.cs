e242dedebfe2236583e2a6e9a026506c3423f7db
Andy Styles <andys@sidefx.com>
2024-04-21 17:48:42 -0400

    Reformat code

--- Plugins/HoudiniEngineUnity/Scripts/VisualScripting/HEU_ModifyHDA.cs
+++ Plugins/HoudiniEngineUnity/Scripts/VisualScripting/HEU_ModifyHDA.cs
@@ -4,58 +4,54 @@ using Unity.VisualScripting;
 
 namespace HoudiniEngineUnity
 {
-
     [UnitCategory("Events/Editor/Houdini Engine")]
     [UnitShortTitle("Modify HDA")]
     [UnitTitle("Houdini Engine Modify HDA")]
     public class HEU_ModifyHDA : ManualEventUnit<EmptyEventArgs>
     {
-	protected override string hookName { get { return "ModifyHDAEvent"; } }
+        protected override string hookName
+        {
+            get { return "ModifyHDAEvent"; }
+        }
 
-	[UnitHeaderInspectable]
-	[HEU_UnitButtonAttribute("TriggerButton", "Modify HDA", 50)]
-	public HEU_UnitButton triggerButton;
+        [UnitHeaderInspectable] [HEU_UnitButtonAttribute("TriggerButton", "Modify HDA", 50)]
+        public HEU_UnitButton triggerButton;
 
-	// Input
-	[DoNotSerialize]
-	public ValueInput inputHDA;
+        // Input
+        [DoNotSerialize] public ValueInput inputHDA;
 
-	// Output
-	[DoNotSerialize]
-	public ValueOutput outputHDAAsset;
+        // Output
+        [DoNotSerialize] public ValueOutput outputHDAAsset;
 
-	// Data
-	private HEU_HoudiniAsset hdaAsset;
+        // Data
+        private HEU_HoudiniAsset hdaAsset;
 
-	protected override void Definition()
-	{
-	    base.Definition();
-	    // Input
-	    inputHDA = ValueInput<HEU_HoudiniAssetRoot>("HDA", null);
+        protected override void Definition()
+        {
+            base.Definition();
+            // Input
+            inputHDA = ValueInput<HEU_HoudiniAssetRoot>("HDA", null);
 
-	    // Output
-	    outputHDAAsset = ValueOutput<HEU_HoudiniAsset>("Output HDA", (flow) => { return hdaAsset; } );
-	}
+            // Output
+            outputHDAAsset = ValueOutput<HEU_HoudiniAsset>("Output HDA", (flow) => { return hdaAsset; });
+        }
 
-	public void TriggerButton(GraphReference reference)
-	{
-	    Flow flow = Flow.New(reference);
-	    HEU_HoudiniAssetRoot hdaAssetRoot = flow.GetValue<HEU_HoudiniAssetRoot>(inputHDA);
+        public void TriggerButton(GraphReference reference)
+        {
+            Flow flow = Flow.New(reference);
+            HEU_HoudiniAssetRoot hdaAssetRoot = flow.GetValue<HEU_HoudiniAssetRoot>(inputHDA);
 
-	    if (hdaAssetRoot != null && hdaAssetRoot.HoudiniAsset != null)
-	    {
-		hdaAsset = hdaAssetRoot.HoudiniAsset;
-	    }
+            if (hdaAssetRoot != null && hdaAssetRoot.HoudiniAsset != null)
+            {
+                hdaAsset = hdaAssetRoot.HoudiniAsset;
+            }
 
-	    System.Action ContinueFlow = () =>
-	    {
-		flow.Invoke(trigger);
-	    };
+            System.Action ContinueFlow = () => { flow.Invoke(trigger); };
 
-	    Debug.Log("Triggered");
+            Debug.Log("Triggered");
 
-	    ContinueFlow();
-	}
+            ContinueFlow();
+        }
     }
 }
 
